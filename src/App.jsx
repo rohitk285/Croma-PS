69c628a0f86022c1f6d70d96e67c8116def16f7a
Rohit <rohitmn482@gmail.com>
2024-10-20 02:48:10 +0530

    NavbarFooter

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,22 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
 import Home from './Home';
 import SelectedCompanies from './SelectedCompanies';  // Import child component
+import Navbar from './utils/Navbar';
+import Footer from './utils/Footer';
 
 const App = () => {
   const [submittedCompanies, setSubmittedCompanies] = useState([]); // Store submitted companies
 
   return (
     <Router>
+      <div className="App flex flex-col min-h-screen">
+        <Navbar />
       <Routes>
         <Route path="/" element={<Home setSubmittedCompanies={setSubmittedCompanies} />} />       
         <Route path="/selected-companies" element={<SelectedCompanies companies={submittedCompanies} />} />
       </Routes>
+      <Footer />
+      </div>
     </Router>
   );
 };
