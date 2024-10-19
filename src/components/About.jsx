6dbdee4a085208d9d41fc556077d66391e0a0722
Rohit <rohitmn482@gmail.com>
2024-10-20 02:37:47 +0530

    About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,10 +9,15 @@ const About = () => {
       <div className="pt-24 md:pt-32 container mx-auto px-4 md:px-8 lg:px-24 flex flex-col-reverse lg:flex-row items-center justify-between">
         <div className="w-full lg:w-1/2 mb-8 lg:mb-0 text-center lg:text-left">
           <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold text-gray-600 mb-4">
-            Get Industry Experience & Paid Internships without ever creating a Resume or Paying for a Course!
+           Chroma Intelligent Solutions
           </h1>
           <h3 className="text-base md:text-lg text-gray-600 mb-6 mt-8">
-            KADAL connects students with startups looking to hire, via live industry projects in domains of Tech, Design & Management, offering hands-on experience and hiring opportunities.
+          Our platform helps you gain competitive insights into India’s
+          electronics retail sector. We provide detailed comparisons of major
+          players in the market, including Reliance Digital, Vijay Sales, Aditya
+          Vision, and more. Whether you’re looking to understand business
+          models, product portfolios, geographical presence, or customer
+          feedback, we’ve got you covered.
           </h3>
           <div className="flex flex-col sm:flex-row sm:space-x-4">
             <a
@@ -21,27 +26,21 @@ const About = () => {
               target="_blank"
               rel="noopener noreferrer"
             >
-              Join the community now →
+              Learn more about our project
             </a>
-            <Link
-              to="/contact?role=organization"
-              className="text-sm w-full sm:w-auto px-6 py-2 rounded-lg md:px-3 md:py-3 text-gray-600 font-semibold hover:bg-gray-400 hover:text-white text-center cursor-pointer"
-            >
-              Hire Instantly →
-            </Link>
           </div>
 
-          <ul className="mt-12 space-y-2 font-semibold text-gray-600 text-sm md:text-base mb-6">
+          {/* <ul className="mt-12 space-y-2 font-semibold text-gray-600 text-sm md:text-base mb-6">
             <li>&#10003; Direct connections to startups.</li>
             <li>&#10003; Real-world projects for hands-on learning.</li>
             <li>&#10003; Mentorship from industry experts.</li>
             <li>&#10003; Paid internships for top performers.</li>
             <li>&#10003; Portfolio-building opportunities.</li>
-          </ul>
+          </ul> */}
         </div>
 
         {/* Image Section */}
-        <div className="w-full lg:w-1/2 flex justify-center">
+        <div className="w-full lg:w-1/3 flex justify-center">
           <div className="relative">
             <img src="../assets/home.png" alt="Illustration" className="max-w-full h-auto" />
             {/* Ensure homeIcon path is correct */}
